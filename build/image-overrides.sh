7a3e056af1dec768146c2e18bb38e38357a34cb1
Rich Renomeron <rrenomeron@gmail.com>
2026-04-28 22:38:58 -0400

    feat: initial attempt at f44 image

--- build/image-overrides.sh
+++ build/image-overrides.sh
@@ -4,16 +4,6 @@ set -eoux pipefail
 
 # Use this for build steps that are unique to this particular image
 
-# Use the version of flatpak that can do preinstalls
-# Remove when Fedora 44 is a thing
-# (Note that this follows the "safe Copr" pattern)
-dnf5 -y copr enable ublue-os/flatpak-test
-dnf5 -y copr disable ublue-os/flatpak-test
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
-dnf5 -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test install flatpak-debuginfo flatpak-libs-debuginfo flatpak-session-helper-debuginfo
-
 # Removes the Fedora flatpak remote with extreme predjudice
 cat > /usr/lib/systemd/system/flatpak-nuke-fedora.service << SERVICE_UNIT
 [Unit]
